b7ecae6d073056c3d3d4b360dba5ace6df805dce
CamilaNevesLinder <camilaneveslindner@.com>
2026-02-23 15:49:03 -0300

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />;
+      <Hero />
     </BrowserRouter>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+function Hero() {
+  return (
+    <section className="relative pt-24">
+      <img
+        src="public/ChatGPT Image 23 de fev. de 2026, 15_27_43.png"
+        className="w-full"
+      />
+
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="bg-white py-7 px-111 hover:scale-110 transition-all duration-300 rounded-2xl shadow-2xl">
+          <h1 className="absolute inset-0 flex items-center justify-center text-5xl uppercase tracking-tight font-bold  ">
+            Sua jornada musical começa aqui
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;

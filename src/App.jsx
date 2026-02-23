e46f359d445e1adf4e09d06b12ec059bafd69b7c
CamilaNevesLinder <camilaneveslindner@.com>
2026-02-23 15:21:32 -0300

    NavBar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />;
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,47 @@
+import { Search } from "lucide-react";
+import { TextAlignJustify } from "lucide-react";
+import { useEffect, useState } from "react";
+
+function NavBar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav
+      className={`fixed w-full top-0 transition-all duration-300 ${
+        scrolled ? "bg-white shadow-md py-3" : "bg-transparent py-6"
+      }`}
+    >
+      <div className="flex  justify-between">
+        <Search className="ml-10 size-8 hover:scale-110 transition-all duration-300 cursor-pointer" />
+        <div className="  flex flex-row justify-center gap-1.5">
+          <h1 className="uppercase font-light text-4xl text-blue-500 tracking-tight  ">
+            praça
+          </h1>
+          <h1 className="uppercase font-extrabold text-4xl text-orange-500 tracking-tight">
+            22
+          </h1>
+        </div>
+        <TextAlignJustify className="mr-10 size-8 hover:scale-110 transition-all duration-300 cursor-pointer" />
+      </div>
+
+      <div></div>
+    </nav>
+  );
+}
+
+export default NavBar;

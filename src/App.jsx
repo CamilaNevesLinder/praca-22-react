4c2eb9879ad9f49439aad69743780bb9e75f6fa6
CamilaNevesLinder <camilaneveslindner@.com>
2026-02-23 18:01:41 -0300

    Intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import Intro from "./components/Intro";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />;
       <Hero />
+      <Intro />
     </BrowserRouter>
   );
 }
---
+++ src/components/Intro.jsx
@@ -0,0 +1,25 @@
+function Intro() {
+  return (
+    <section className="flex justify-center bg-[#031229]">
+      <div className="grid grid-cols-3 gap-15 mt-10 ">
+        <div className="ml-10 mb-5">
+          <img
+            src="public/WhatsApp Image 2025-12-05 at 11.34.57.jpeg"
+            className=""
+          />
+        </div>
+        <div className="bg-white py-20 px-10 mb-5">
+          <img
+            src="public/WhatsApp Image 2025-12-05 at 11.34.57 (1).jpeg"
+            className=""
+          />
+        </div>
+        <div className="bg-white mr-10 mb-5">
+          <img src="public/WhatsApp Image 2025-12-05 at 11.34.55 (1).jpeg" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Intro;
